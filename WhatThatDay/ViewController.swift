eeb098d0340f132b7d72f9c2eab9696c0869f25f
Mostafa <MostafaAbdEl_Fatah@192.168.179.128>
2017-10-07 09:32:09 -0700

    PikerDateDemo

--- WhatThatDay/ViewController.swift
+++ WhatThatDay/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func DisplayDay(sender: UIButton) {
+        let chosenDate = self.datePicker.date
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "EEEE"
+        let day = formatter.stringFromDate(chosenDate)
+        let result = "The Day is \(day)"
+        let myAlter = UIAlertController(title: result, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        myAlter.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(myAlter , animated: true, completion: nil)
+    }
 
 }
 
